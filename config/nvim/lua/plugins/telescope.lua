2c73fe04ddd0c0e717eb2071296e092a3dbb373f
Pandya S. <me@saka.my.id>
2025-08-20 23:25:31 +0700

    Fix indenting

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,25 +1,25 @@
 -- plugins/telescope.lua
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    require('telescope').setup({
-      defaults = {
-        -- nothing needed here for follow
-      },
-      pickers = {
-        find_files = {
-          follow = true, -- telescope level
-          hidden = true, -- if you also want hidden files
-        },
-      },
-    })
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        require('telescope').setup({
+            defaults = {
+                -- nothing needed here for follow
+            },
+            pickers = {
+                find_files = {
+                    follow = true, -- telescope level
+                    hidden = true, -- if you also want hidden files
+                },
+            },
+        })
 
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-  end
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    end
 }
 
